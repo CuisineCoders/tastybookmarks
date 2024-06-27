344197b913ed2218c3b7c64d390edfba5f46767c
Michael Zimmermann <info@michaelzimmermann.com>
2024-06-27 21:23:41 +0200

    Add API testing component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecipeTesterComponent } from './recipe-tester/recipe-tester.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RecipeTesterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
