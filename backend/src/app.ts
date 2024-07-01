4a2cf1bf497080c78408e49acaf8e036d7ff1711
Michael Zimmermann <info@michaelzimmermann.com>
2024-07-01 20:47:58 +0200

    Add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import { recipeRouter } from './routes/recipeRouter';
 
 export const app: Application = express();
+
+app.use(cors());
 
 app.use('/api/recipes', recipeRouter);
