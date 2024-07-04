9fbfa6bed41b185d4c0a3498e27571809561bfb4
Michael Zimmermann <info@michaelzimmermann.com>
2024-07-04 20:49:53 +0200

    Integrate main branch

--- frontend/src/tasty-main/tasty.component.ts
+++ frontend/src/tasty-main/tasty.component.ts
@@ -1,10 +1,11 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'tasty-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent],
   templateUrl: './tasty.component.html',
   styleUrl: './tasty.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
