9c78e7498f903361385cb7ad67afc468d440522c
Michael Zimmermann <info@michaelzimmermann.com>
2024-07-07 21:20:32 +0200

    Add FAB material button

--- frontend/src/tasty-main/tasty.component.ts
+++ frontend/src/tasty-main/tasty.component.ts
@@ -1,13 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'tasty-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule],
   templateUrl: './tasty.component.html',
   styleUrl: './tasty.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TastyComponent {
+
+  testFab() {
+    console.log('FAB Test');
+  }
 }
