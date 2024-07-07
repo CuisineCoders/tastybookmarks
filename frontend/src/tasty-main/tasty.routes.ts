45fde3cbca69c74cb4e492140a98c01760e92acb
Michael Zimmermann <info@michaelzimmermann.com>
2024-07-07 16:30:47 +0200

    Show recipe-tester via route

--- frontend/src/tasty-main/tasty.routes.ts
+++ frontend/src/tasty-main/tasty.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RecipeTesterComponent } from './recipe-tester/recipe-tester.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'recipe-tester', component: RecipeTesterComponent },
+];
