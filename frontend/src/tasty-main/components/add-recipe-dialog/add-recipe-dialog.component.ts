2b8f1af622501d0b3aee569f6679e274a5fce5ff
Michael Zimmermann <info@michaelzimmermann.com>
2024-07-27 23:23:55 +0200

    Add url input validator

--- frontend/src/tasty-main/components/add-recipe-dialog/add-recipe-dialog.component.ts
+++ frontend/src/tasty-main/components/add-recipe-dialog/add-recipe-dialog.component.ts
@@ -1,19 +1,29 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroupDirective, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'tasty-add-recipe-dialog',
   standalone: true,
-  imports: [FormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatDialogModule, MatIconModule],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatDialogModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './add-recipe-dialog.component.html',
-  styleUrl: './add-recipe-dialog.component.scss',
+  styleUrls: ['./add-recipe-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AddRecipeDialogComponent {
-  protected url: string | undefined;
+  private regEx = /^(?:http(s)?:\/\/)?[a-zA-ZäöüÄÖÜß0-9.-]+(?:\.[a-zA-ZäöüÄÖÜß0-9\.-]+)+[a-zA-ZäöüÄÖÜß0-9\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+  protected urlFormControl = new FormControl('', [Validators.required, Validators.pattern(this.regEx)]);
+  protected matcher = new MyErrorStateMatcher();
 }
