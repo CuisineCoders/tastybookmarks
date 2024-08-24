b0897974028a4fb8c483cce0d5adb5185ab9db49
Michael Zimmermann <info@michaelzimmermann.com>
2024-08-24 12:27:39 +0200

    Add model

--- backend/src/model/recipe.ts
+++ backend/src/model/recipe.ts
@@ -1,6 +1,34 @@
+export interface NutritionInformation {
+    servingSize?: number;
+    calories?: string;
+    proteinContent?: string;
+    fatContent?: string;
+    carbohydrateContent?: string;
+}
+
+export interface VideoObject {
+    name?: string;
+    description?: string;
+    thumbnailUrl?: string;
+    contentUrl?: string;
+    embedUrl?: string;
+    uploadDate?: string;
+}
+
 export interface Recipe {
     id: string;
-    name: string,
-    ingredients: string,
-    data: any; // Anpassen, wenn das Schema bekannt ist
+    url: string;
+    name: string;
+    description: string;
+    image: string;
+    keywords: string[];
+    prepTime: string;
+    cookTime?: string | null;
+    totalTime: string;
+    recipeCategory: string;
+    recipeIngredient: string[];
+    recipeInstructions: string[];
+    recipeYield: number;
+    nutrition?: NutritionInformation | null;
+    video?: VideoObject | null;
 };
