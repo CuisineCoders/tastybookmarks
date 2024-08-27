a258e8535d1547bfb0c653549f8308ea2073f559
Jan0H0Punkt <108615195+Jan0H0Punkt@users.noreply.github.com>
2024-08-27 22:21:08 +0200

    Adjusted model in frontend

--- frontend/src/tasty-main/model/recipe.ts
+++ frontend/src/tasty-main/model/recipe.ts
@@ -1,8 +1,9 @@
 export interface NutritionInformation {
-  calories: string;
-  proteinContent: string;
-  fatContent: string;
-  carbohydrateContent: string;
+  servingSize?: number
+  calories?: string;
+  proteinContent?: string;
+  fatContent?: string;
+  carbohydrateContent?: string;
 }
 
 export interface VideoObject {
@@ -18,18 +19,16 @@ export interface Recipe {
   id: string;
   url: string;
   name: string;
-  description: string;
+  description?: string;
   imageUrl?: string;
-  keywords: Array<string>;
+  keywords?: Array<string>;
   prepTime?: string;
   cookTime?: string;
-  totalTime: string;
-  category: string;
+  totalTime?: string;
+  category?: string;
   ingredient: Array<string>;
-  instructions: Array<string>;
-  servingSize: number
+  instructions: Array<Array<string>>;
+  servings: number
   nutrition?: NutritionInformation;
   video?: VideoObject;
 }
-
-export interface RecipePreview extends Pick<Recipe, 'id' | 'name' | 'imageUrl'> {}
