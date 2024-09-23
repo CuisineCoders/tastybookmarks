cae259123e48722204376939788688dcff81d37a
Jan0H0Punkt <108615195+Jan0H0Punkt@users.noreply.github.com>
2024-09-23 17:33:06 +0200

    WIP: added delete button to list items

--- frontend/src/tasty-main/components/recipe-list/recipe-list.component.ts
+++ frontend/src/tasty-main/components/recipe-list/recipe-list.component.ts
@@ -1,31 +1,39 @@
-import {CommonModule} from '@angular/common';
-import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
-import {RouterLink} from '@angular/router';
-import {Observable} from 'rxjs';
-import {DummyRecipeApiService, RecipeApiService} from '../../services';
-import {KebabCasePipe} from '../../pipes';
-import {Recipe} from "../../model";
-import {TastyFabControl} from "../../services/fab-control.service";
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DummyRecipeApiService, RecipeApiService } from '../../services';
+import { KebabCasePipe } from '../../pipes';
+import { Recipe } from '../../model';
+import { TastyFabControl } from '../../services/fab-control.service';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
-  selector: 'tasty-list',
-  standalone: true,
-  imports: [
+  selector:        'tasty-list',
+  standalone:      true,
+  imports:         [
     CommonModule,
     RouterLink,
-    KebabCasePipe
+    KebabCasePipe,
+    MatIcon,
+    MatIconButton,
   ],
-  templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.scss',
+  templateUrl:     './recipe-list.component.html',
+  styleUrl:        './recipe-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [{provide: RecipeApiService, useClass: DummyRecipeApiService}]
+  providers:       [{ provide: RecipeApiService, useClass: DummyRecipeApiService }],
 })
 export class RecipeListComponent implements OnInit {
-  private readonly _fabControl = inject(TastyFabControl)
+  private readonly _fabControl = inject(TastyFabControl);
 
-  protected _recipes$: Observable<Array<Recipe>> = inject(RecipeApiService).getAllRecipes()
+  protected _recipes$: Observable<Array<Recipe>> = inject(RecipeApiService).getAllRecipes();
 
   public ngOnInit(): void {
-    this._fabControl.displayButtons = [{option: 'AddRecipeButton'}]
+    this._fabControl.displayButtons = [{ option: 'AddRecipeButton' }];
+  }
+
+  protected delete(id: string): void {
+    console.log('delete ' + id);
   }
 }
