f37c46181787fdbec0eb2b9e75e8c51fa93105d4
Michael Zimmermann <info@michaelzimmermann.com>
2024-10-19 11:33:48 +0200

    Difference between local and live DB connection

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -7,14 +7,21 @@ export async function connectDB(): Promise<void> {
     if (isConnected) {
         return;
     }
+
+    const mongoUri = process.env.NODE_ENV === 'production'
+        ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+        : `mongodb://${process.env.MONGO_HOST_LOCAL}/${process.env.MONGO_DB_NAME}`;
+
+    if (!mongoUri) {
+        throw new Error('MongoDB URI is not defined');
+    }
+
     try {
-        // await mongoose.connect('mongodb://127.0.0.1:27017/tastybookmarks');
-        const mongoURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
-        await mongoose.connect(mongoURI);
+        await mongoose.connect(mongoUri);
         isConnected = true;
-        console.log('Successfully connected to MongoDB.');
+        console.log(`Successfully connected to MongoDB: ${mongoUri}`);
     } catch (error) {
         console.error('Failed to connect to MongoDB: ', error);
-        process.exit(1); // Optional, je nach Fehlermanagement
+        process.exit(1);
     }
 }
