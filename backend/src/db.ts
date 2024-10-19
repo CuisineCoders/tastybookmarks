fe5c41431b438a06bae0af8c27c52c4e1f9a6a4b
Michael Zimmermann <info@michaelzimmermann.com>
2024-10-19 19:03:24 +0200

    Test db.ts MONGO_CONTEXT

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -8,11 +8,9 @@ export async function connectDB(): Promise<void> {
         return;
     }
 
-    // const mongoUri = process.env.NETLIFY === 'true'
-    //     ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
-    //     : `mongodb://${process.env.MONGO_HOST_LOCAL}/${process.env.MONGO_DB_NAME}`;
-
-    const mongoUri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+    const mongoUri = process.env.MONGO_CONTEXT === 'production'
+        ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+        : `mongodb://${process.env.MONGO_HOST_LOCAL}/${process.env.MONGO_DB_NAME}`;
 
     if (!mongoUri) {
         throw new Error('MongoDB URI is not defined');
