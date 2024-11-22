e14008a78943a6b2b4975dd3391fb7968c0e2949
Jan0H0Punkt <108615195+Jan0H0Punkt@users.noreply.github.com>
2024-11-22 12:37:35 +0100

    bottom navbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FabControlComponent } from './core/fab-control/fab-control.component';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs';
 
 @Component({
   selector:        'tasty-root',
   templateUrl:     './app.component.html',
   styleUrl:        './app.component.scss',
-  imports: [CommonModule, RouterOutlet, RouterLink, FabControlComponent],
+  imports:         [CommonModule, RouterOutlet, RouterLink, FabControlComponent],
   standalone:      true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  protected readonly _screenIsSmall = inject(BreakpointObserver).observe('(max-width: 575px)')
+    .pipe(map((event) => event.matches));
 }
