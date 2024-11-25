a5bad6ad55128285ce9b0b6a2e628ada8c252d6d
Jan0H0Punkt <108615195+Jan0H0Punkt@users.noreply.github.com>
2024-11-25 21:15:56 +0100

    maintain(FAB control): alternative placement and footer navbar, so we don't have the FAB container on top of other stuff that is no longer interactable (#32)

--- frontend/src/app/recipe/components/detail/recipe-detail.component.ts
+++ frontend/src/app/recipe/components/detail/recipe-detail.component.ts
@@ -3,17 +3,18 @@ import { Observable, switchMap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { TastyFabControl } from '../../../core/fab-control/fab-control.service';
 import { MatDialog } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
 import { DeleteConfirmationDialogComponent } from '../delete-confirmation-dialog/delete-confirmation-dialog.component';
 import { filter, tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { Recipe } from '../../model/recipe';
 import { RecipeApiService } from '../../services/recipe-api.service';
+import { AddRecipeDialogComponent } from '../add-dialog/add-recipe-dialog.component';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'tasty-recipe-detail',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIcon],
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -30,6 +31,17 @@ export class RecipeDetailComponent implements OnInit {
 
   public ngOnInit(): void {
     this._fabControl.displayButtons = [
+      {
+        option:      'AddRecipeButton',
+        clickAction: () => this._dialog.open<AddRecipeDialogComponent, null, string | undefined>(
+          AddRecipeDialogComponent, { width: '450px' })
+          .afterClosed()
+          .pipe(
+            filter(event => event !== undefined),
+            switchMap(result => this._recipeApiService.addRecipe(result)),
+          )
+          .subscribe(),
+      },
       {
         option: 'DeleteRecipeButton',
         clickAction: () => this._dialog.open(DeleteConfirmationDialogComponent).afterClosed()
