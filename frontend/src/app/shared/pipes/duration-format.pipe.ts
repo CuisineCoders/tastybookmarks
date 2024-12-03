438ef3b5db88561661ffc60079052195cccbbc80
Michael Zimmermann <info@michaelzimmermann.com>
2024-12-03 21:00:15 +0100

    Change pipe after PR

--- frontend/src/app/shared/pipes/duration-format.pipe.ts
+++ frontend/src/app/shared/pipes/duration-format.pipe.ts
@@ -7,38 +7,27 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DurationFormatPipe implements PipeTransform {
 
   transform(value: string): string {
-    if (!value || !value.startsWith('P')) {
-      return value; // Falls die Eingabe ungültig ist, einfach zurückgeben
+    if (!value.startsWith('P')) {
+      return value;
     }
 
-    // Regex zur Extraktion von Tagen, Stunden, Minuten und Sekunden
-    const regex = /P(?:(\d+)D)?T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/;
-    const matches = value.match(regex);
+    const regexToExtractDuration = /P(?:(\d+)D)?T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/;
+    const matches = value.match(regexToExtractDuration);
 
     if (!matches) {
       return value; // Rückgabe der Originalzeichenkette bei Fehler
     }
 
-    const days = parseInt(matches[1] || '0', 10);
-    const hours = parseInt(matches[2] || '0', 10);
-    const minutes = parseInt(matches[3] || '0', 10);
-    const seconds = parseInt(matches[4] || '0', 10);
+    const units = [
+      { value: parseInt(matches[1] || '0', 10), singular: 'Tag', plural: 'Tage' },
+      { value: parseInt(matches[2] || '0', 10), singular: 'Stunde', plural: 'Stunden' },
+      { value: parseInt(matches[3] || '0', 10), singular: 'Minute', plural: 'Minuten' },
+      { value: parseInt(matches[4] || '0', 10), singular: 'Sekunde', plural: 'Sekunden' },
+    ];
 
-    // Aufbau der Ausgabekette
-    const parts: string[] = [];
-    if (days > 0) {
-      parts.push(`${days} Tag${days > 1 ? 'e' : ''}`);
-    }
-    if (hours > 0) {
-      parts.push(`${hours} Stunde${hours > 1 ? 'n' : ''}`);
-    }
-    if (minutes > 0) {
-      parts.push(`${minutes} Minute${minutes > 1 ? 'n' : ''}`);
-    }
-    if (seconds > 0) {
-      parts.push(`${seconds} Sekunde${seconds > 1 ? 'n' : ''}`);
-    }
-
-    return parts.join(' ');
+    return units
+      .filter(({ value }) => value > 0)
+      .map(({ value, plural, singular }) => `${value} ${value > 1 ? plural : singular}`)
+      .join(' ');
   }
 }
