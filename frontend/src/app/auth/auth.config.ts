1dcaf510e413a4801a7a7d98000ab65d3690e8e5
Jan0H0Punkt <108615195+Jan0H0Punkt@users.noreply.github.com>
2025-01-13 21:34:09 +0100

    first steps with aws cognito

--- frontend/src/app/auth/auth.config.ts
+++ frontend/src/app/auth/auth.config.ts
@@ -3,7 +3,7 @@ import { PassedInitialConfig } from 'angular-auth-oidc-client';
 export const authConfig: PassedInitialConfig = {
   config: {
               authority: 'https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_btB1jIcvb',
-              redirectUrl: 'https://tastybookmarks.netlify.app/',
+              redirectUrl: 'https://frontend-login--tastybookmarks.netlify.app/login',
               postLogoutRedirectUri: window.location.origin,
               clientId: '21576l0d4q7bfgnc4b8jpd0v3v',
               scope: 'openid profile email', // 'openid profile offline_access ' + your scopes
