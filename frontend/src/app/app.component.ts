60f8342994eb0a0122109207eb969eb3f205d3bc
Michael Zimmermann <info@michaelzimmermann.com>
2025-02-06 10:13:48 +0100

    Deactivate ChangeStrategy OnPush

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,22 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Amplify } from 'aws-amplify';
-
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId: 'eu-north-1_btB1jIcvb',
-      userPoolClientId: '21576l0d4q7bfgnc4b8jpd0v3v',
-    }
-  }
-});
 
 @Component({
   selector: 'tasty-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
   template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent { }
--- frontend/src/app/core/login/login.component.ts
+++ frontend/src/app/core/login/login.component.ts
@@ -1,7 +1,17 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 // import { AuthService } from '../services/auth.service';
+import { Amplify } from 'aws-amplify';
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'eu-north-1_btB1jIcvb',
+      userPoolClientId: '21576l0d4q7bfgnc4b8jpd0v3v',
+    }
+  }
+});
 
 @Component({
   selector:        'tasty-login',
@@ -9,7 +19,7 @@ import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
   imports:         [MatButtonModule, AmplifyAuthenticatorModule],
   templateUrl:     './login.component.html',
   styleUrl:        './login.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
   // private readonly authService = inject(AuthService);
