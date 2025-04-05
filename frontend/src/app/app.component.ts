c6b0748ecce7bd57da0f156c285edf49f702137f
Jan0H0Punkt <108615195+Jan0H0Punkt@users.noreply.github.com>
2025-04-05 19:59:42 +0200

    wip

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,12 +4,16 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavigationControlComponent } from './core/nav-control/navigation-control.component';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { map } from 'rxjs';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @Component({
   selector:        'tasty-root',
   templateUrl:     './app.component.html',
   styleUrl:        './app.component.scss',
   imports:         [CommonModule, RouterOutlet, RouterLink, NavigationControlComponent],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
+  ],
   standalone:      true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
