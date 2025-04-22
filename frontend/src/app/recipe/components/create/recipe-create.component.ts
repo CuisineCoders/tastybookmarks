3c2a863b338ed7232a6b40d9f9d442039659deaa
Jan0H0Punkt <108615195+Jan0H0Punkt@users.noreply.github.com>
2025-04-22 11:10:41 +0200

    wip

--- frontend/src/app/recipe/components/create/recipe-create.component.ts
+++ frontend/src/app/recipe/components/create/recipe-create.component.ts
@@ -6,6 +6,7 @@ import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { isNullOrUndefined } from '../../../utils/type-checks';
 import { map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector:        'tasty-recipe-create',
@@ -20,6 +21,7 @@ import { map } from 'rxjs';
     MatSuffix,
     MatButton,
     MatError,
+    AsyncPipe,
   ],
   templateUrl:     './recipe-create.component.html',
   styleUrl:        './recipe-create.component.scss',
@@ -40,7 +42,7 @@ export class RecipeCreateComponent {
     name:        this.name,
     description: new FormControl(undefined),
 
-    servingSize: new FormControl('1', [Validators.required, Validators.min(1), numberValidator]),
+    servingSize: new FormControl('1', [Validators.min(1), numberValidator, Validators.required]),
     ingredients: this.ingredients,
     steps:       this.steps,
 
@@ -55,13 +57,23 @@ export class RecipeCreateComponent {
     categories: new FormControl('', Validators.required),
   });
 
-  protected isNameInvalid = this.name.events.pipe(map(({ source }) => source.touched && source.invalid));
+  protected isNameInvalid$ = this.name.events.pipe(map(({ source }) => source.hasError('required')));
+  protected servingSizeError$ = this.createRecipeForm.get('servingSize')?.events
+    ?.pipe(map(({ source }) => checkForNumberMinRequiredError(source, 'Portionsgröße')));
+  protected prepTimeError$ = this.createRecipeForm.get('prepTime')?.events
+    ?.pipe(map(({ source }) => checkForNumberMinRequiredError(source, 'Zubereitungszeit')));
+  protected cockTimeError$ = this.createRecipeForm.get('cookTime')?.events
+    ?.pipe(map(({ source }) => checkForNumberMinRequiredError(source, 'Kochzeit')));
 
   protected save(): void {
     console.log(`Save recipe: ${JSON.stringify(this.createRecipeForm.value)}`);
-    console.log(this.createRecipeForm.get('name')?.invalid);
   }
 }
+
+const checkForNumberMinRequiredError = (source: AbstractControl, entity: string): string | undefined => [
+  { error: 'required', message: `Du musst eine ${entity} angeben!` },
+  { error: 'min', message: `Du kannst keine kleinere ${entity} als 1 angeben!` },
+].find(({ error }) => source.hasError(error))?.message;
 
 const numberValidator = Validators.pattern(/^\d+$/);
 const optionalNumberValidator = (control: AbstractControl) => isNullOrUndefined(control.value) ? null : numberValidator(
